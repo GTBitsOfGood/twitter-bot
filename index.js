a8c13a783594f85c80428fa785165e3a956174a8
Max Karpawich <max.karpawich@gmail.com>
2020-03-09 18:24:23 -0400

    challenge 11

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
+const twitter = require('twitter')
 
 // initialize environment variables from your .env file
 dotenv.config()
@@ -14,6 +15,25 @@ const auth = {
 }
 
 const app = express()
+
+/**
+ * Send a tweet to the Twitter API
+ * 
+ * https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/post-statuses-update
+ */
+function sendTweet(tweet) {
+
+    // see the Parameters section of the Twitter API docs linked above ^^^
+    const params = {
+        status: tweet
+    }
+
+    twitter.post('statuses/update', params, (errors, data, response) => {
+        if (errors) {
+            console.log(errors) // logs errors to console so we can debug what went wrong
+        }
+    })
+}
 
 function tweetHandler(request, response) {
     // future code here
