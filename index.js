462c9029a486661a5e252a4e4456504d450b5b4f
Max Karpawich <max.karpawich@gmail.com>
2020-03-09 19:00:59 -0400

    challenge 6

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function tweetHandler(request, response) {
     // future code here
 }
 
-app.post('/tweet', tweetHandler)
-
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.post('/tweet', tweetHandler)
 
 app.listen(3000) // the port does not matter - we chose 3000 randomly
